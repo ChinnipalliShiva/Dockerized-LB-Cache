78df3a93b16c5e1583843cd0d721f50ed8068f51
Chinnipalli Shiva <chinnipallishiva26@gmail.com>
2025-05-11 09:31:49 +0530

    Committing file: loadBalancer\childServers\serverConfig.js

---
+++ loadBalancer/childServers/serverConfig.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: "server1",
+    host: "localhost",
+    port: 3001,
+  },
+  {
+    name: "server2",
+    host: "localhost",
+    port: 3002,
+  },
+  {
+    name: "server3",
+    host: "localhost",
+    port: 3003,
+  },
+];
