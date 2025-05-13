ef844fc4145063902a07a45123a56b9aa2e2e32a
Chinnipalli Shiva <chinnipallishiva26@gmail.com>
2025-05-13 16:23:11 +0530

    Committing file: loadBalancer\childServers\sqlConnection\sqlConnection.js

---
+++ loadBalancer/childServers/sqlConnection/sqlConnection.js
@@ -0,0 +1,36 @@
+const mysql = require("mysql2/promise");
+
+//dockerized
+let pool;
+
+async function sqlConnection() {
+  if (pool) return pool; // Reuse the pool if already created
+
+  try {
+    pool = mysql.createPool({
+      // host: process.env.DB_HOST || "localhost",
+      // port: process.env.DB_PORT || 8000,
+
+      host: process.env.DB_HOST || "sql-container",
+      port: process.env.DB_PORT || 3306,
+      user: process.env.DB_USER || "testuser",
+      password: process.env.DB_PASSWORD || "testpass",
+      database: process.env.DB_NAME || "testdb",
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+
+    // Test a connection
+    await pool.getConnection();
+    console.log("✅ Connected to MySQL (pooled)");
+
+    return pool;
+  } catch (err) {
+    console.error("❌ Failed to connect to MySQL:", err.message);
+    throw new Error("MySQL pool connection failed");
+  }
+}
+module.exports = {
+  sqlConnection,
+};
