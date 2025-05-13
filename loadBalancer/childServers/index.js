ed25c0b785452e60a32bbfc40ad93a583c66fcc4
Chinnipalli Shiva <chinnipallishiva26@gmail.com>
2025-05-13 16:23:07 +0530

    Committing file: loadBalancer\childServers\index.js

--- loadBalancer/childServers/index.js
+++ loadBalancer/childServers/index.js
@@ -1,27 +1,63 @@
 const express = require("express");
 const servers = require("./serverConfig");
+const { sqlConnection } = require("./sqlConnection/sqlConnection");
+const { fetchUserFromSql } = require("./sqlCrud/fetchUserFromSql");
 
-const createServer = () => {
+let sqlClient;
+
+// Initialize MySQL Connection (once for all servers)
+async function initializeConnections() {
+  try {
+    sqlClient = await sqlConnection();
+    console.log("✅ Connected to SQL");
+  } catch (error) {
+    console.error("❌ Error initializing SQL connection:", error.message);
+    process.exit(1);
+  }
+}
+
+// Graceful Shutdown
+async function closeConnections() {
+  try {
+    if (sqlClient) await sqlClient.end();
+    console.log("✅ SQL connection closed");
+    process.exit(0);
+  } catch (error) {
+    console.error("❌ Error during shutdown:", error.message);
+    process.exit(1);
+  }
+}
+
+// Create Servers
+const createServers = async () => {
+  await initializeConnections();
   for (let server of servers) {
     const app = express();
     app.use(express.json());
-    app.listen(server.port, () => {
-      console.log(`Server is running on port ${server.port}`);
+    app.post("/child/getUser", async (req, res) => {
+      try {
+        const userData = await fetchUserFromSql(sqlClient, req.body); // optionally get params from req.query
+        if (userData.status === "error" && userData.data === null) {
+          console.error("❌ User not found in SQL");
+          return res
+            .status(200)
+            .json({ success: false, message: userData.message });
+        }
+        res.status(200).json({ success: true, data: userData });
+      } catch (error) {
+        console.error("❌ Error fetching user data:", error.message);
+        res
+          .status(500)
+          .json({ success: false, message: "Internal Server Error" });
+      }
     });
-
-    app.get("/data", (req, res) => {
-      console.log("req.body child", req.body);
-      res.json({
-        message: `Hello from ${server.name}`,
-        data: {
-          serverName: server.name,
-          serverHost: server.host,
-          serverPort: server.port,
-          userInfo: req.body.user || null,
-        },
-      });
+    app.listen(server.port, () => {
+      console.log(`🚀 Server is running on port ${server.port}`);
     });
   }
+  // Set shutdown handler only once
+  process.once("SIGINT", closeConnections);
+  process.once("SIGTERM", closeConnections);
 };
 
-createServer();
+createServers();
