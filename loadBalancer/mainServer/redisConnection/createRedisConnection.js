4ad6afc3cb3f41709e1d7aad68abb30b7d18ca7c
Chinnipalli Shiva <chinnipallishiva26@gmail.com>
2025-05-13 16:23:33 +0530

    Committing file: loadBalancer\mainServer\redisConnection\createRedisConnection.js

---
+++ loadBalancer/mainServer/redisConnection/createRedisConnection.js
@@ -0,0 +1,26 @@
+const redis = require("redis");
+
+const createRedisConnection = async () => {
+  //docker network
+  const client = redis.createClient({
+    url: "redis://redis-container:6379",
+  });
+
+  //localhost
+  // const client = redis.createClient({
+  //   url: process.env.REDIS_URL || "redis://localhost:6379",
+  // });
+
+  try {
+    await client.connect();
+    console.log("✅ Connected to Redis");
+    return client;
+  } catch (error) {
+    console.error("❌ Redis connection error:", error.stack || error.message);
+    throw new Error("Redis connection failed");
+  }
+};
+
+module.exports = {
+  createRedisConnection,
+};
